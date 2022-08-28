90aa530e7da1b5ddce293042dfa97e319851a7ca
Alexander <kisliy.doma@gmail.com>
2022-08-28 16:14:34 +0800

    showing a random character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,12 +12,26 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {                              /* функция получения всех персонажей */
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=100&${this._apiKey}`);
+    getAllCharacters = async () => {                        /* функция получения всех персонажей */
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=100&${this._apiKey}`);
+
+        return res.data.results.map(this._transformCaracter);
     }
 
-    getCharacter = (id) => {                                /* функция получения конкретного персонажа */
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {                          /* функция получения конкретного персонажа */
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+
+        return this._transformCaracter(res.data.results[0]);
+    }
+
+    _transformCaracter = (char) => {                        /* функция трансформации объекта с данными персонажа в объект с только нужными свойствами */
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
